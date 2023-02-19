dacca8f8e58d2e88e9a960e216520fe2ec4902cc
yusong1 <yusong1@jushuitan.com>
2023-02-19 22:32:04 +0800

    add factory

---
+++ FactoryPatter/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace FactoryPatter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Factory factory = new HuaWeiFactory();
+            Phone phone=  factory.createPhone();
+            phone.print();
+
+
+
+            Factory factory2 = new IPhoneFactory();
+            Phone phone2 = factory2.createPhone();
+            phone2.print();
+            Console.ReadKey();
+        }
+    }
+
+    //抽象手机
+    public interface Phone
+    {
+        void print();
+    
+    };
+    //具体手机 苹果手机
+    public class iPhone : Phone
+    {
+        public void print()
+        {
+            Console.WriteLine("苹果手机");
+        }
+    }
+    //具体手机 华为手机
+    public class HuaWeiPhone : Phone
+    {
+        public void print()
+        {
+            Console.WriteLine("华为手机");
+        }
+    }
+    //具体手机 小米手机
+    public class Xiaomi : Phone
+    {
+        public void print()
+        {
+            Console.WriteLine("小米手机");
+        }
+    }
+
+   //抽象工厂
+    public interface Factory
+    {
+        Phone createPhone();
+    }
+
+    //具体工厂 苹果工厂
+    public class IPhoneFactory : Factory
+    {
+        public Phone createPhone()
+        {
+            return new iPhone();
+        }
+    }
+    //具体工厂 华为工厂
+    public class HuaWeiFactory : Factory
+    {
+        public Phone createPhone()
+        {
+            return new HuaWeiPhone();
+        }
+    }
+    //具体工厂 小米工厂
+    public class XiaomiFactory : Factory
+    {
+        Phone Factory.createPhone()
+        {
+            return new Xiaomi();
+        }
+    }
+}
--- ProSingletonStatic/Program.cs
+++ ProSingletonStatic/Program.cs
@@ -6,7 +6,15 @@ namespace ProSingletonStatic
     {
         static void Main(string[] args)
         {
+            Singleton.x = 100;
+            Singleton.y = 200;
             var ins = Singleton.instance;
+
+            ins.Method("Test");
+
+            Singleton.x = 1000;
+            Singleton.y = 2000;
+
             ins.Method("Test");
         }
     }
@@ -17,17 +25,28 @@ namespace ProSingletonStatic
 
         public static readonly Singleton instance;
 
+        public static int x;
+        public static int y;
+        //即使是多个线程下，也是可以保证只有一个instance,因为静态构造函数，只执行一次
         static Singleton()
         {
-            instance = new Singleton();
+            if (instance == null)
+            {
+
+                instance = new Singleton(x, y);
+            }
         }
 
-        private Singleton() { }
+        private Singleton(int x, int y)
+        {
+            Singleton.x = x;
+            Singleton.y = y;
+        }
 
 
         public void Method(string name)
         {
-            Console.WriteLine(name);
+            Console.WriteLine($"x={x},y={y}");
         }
     }
 }
