e643f634837d9a964e9a37d97211813e9c59f0ba
zhukb <912549963@qq.com>
2023-12-25 22:45:01 +0800

    app.UseFastEndpoints();

--- FastEndpointsWebApi/Program.cs
+++ FastEndpointsWebApi/Program.cs
@@ -30,9 +30,13 @@ namespace FastEndpointsWebApi
 
             var app = builder.Build();
 
+
+
+            app.UseFastEndpoints();
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
+
                 app.UseSwagger();
                 app.UseSwaggerUI(c => {
                     c.SwaggerEndpoint("/swagger/v1/swagger.json", "API V1");
