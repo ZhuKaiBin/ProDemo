3b6f29cd468f7489a6fcdea85a44b1acabc95391
ZT158\dyzhukb <dyzhukb@chint.com>
2024-01-09 08:54:06 +0800

    add

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,21 @@
+﻿using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Runtime;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+
+        /*
+         CommandMethod的特性，CAD在运行的时候会搜索所有有CommandMethod特性的方法来作为命令入口。
+          所以如果在CAD中输入SayHello命令就会执行该方法内的动作。
+         */
+        [CommandMethod("SayHello")]
+        public void SayHello()
+        {
+            Editor editor = Application.DocumentManager.MdiActiveDocument.Editor;
+            editor.WriteMessage("Hello World");
+        }
+    }
+}
---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,39 @@
+﻿using Aspose.CAD;
+using Aspose.CAD.FileFormats.Cad;
+using Aspose.CAD.ImageOptions;
+
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string dwgFilePath = @"C:\Users\dyzhukb\Desktop\CADDemo\5320db1794en.dxf";
+            string svgFilePath = @"C:\Users\dyzhukb\Desktop\CADDemo";
+
+            try
+            {
+                // 加载 DWG 文件
+                using (var cadImage = (CadImage)Image.Load(dwgFilePath))
+                {
+                    // 设置输出文件格式为 SVG
+                    var options = new CadRasterizationOptions
+                    {
+                        PageWidth = 1600, // 设置输出 SVG 的页面宽度
+                        PageHeight = 1200 // 设置输出 SVG 的页面高度
+                    };
+
+                    // 将 DWG 文件转换为 SVG 文件
+                    cadImage.Save(svgFilePath);
+                }
+
+                System.Console.WriteLine("DWG 文件已成功转换为 SVG 文件。");
+            }
+            catch (System.Exception ex)
+            {
+                System.Console.WriteLine("转换过程中发生错误：" + ex.Message);
+            }
+        }
+    }
+}
