27f10df00ff773fd88c657049aac627c5b768035
zhukb <912549963@qq.com>
2024-01-10 23:53:19 +0800

    add

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Net;
+using HtmlAgilityPack;
+
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string keyword = "销售";
+            string url = "https://www.zhipin.com/web/geek/job?query=%E7%A0%94%E5%8F%91&city=101020100";
+
+            // 创建Web客户端
+            WebClient client = new WebClient();
+            client.Encoding = System.Text.Encoding.UTF8;
+
+            // 下载页面内容
+            string html = client.DownloadString(url);
+
+            // 使用HtmlAgilityPack解析HTML
+            HtmlDocument doc = new HtmlDocument();
+            doc.LoadHtml(html);
+
+            // 获取岗位列表
+            HtmlNodeCollection jobNodes = doc.DocumentNode.SelectNodes("//div[@class='listBox']/ul/li");
+
+            // 遍历岗位列表
+            foreach (HtmlNode jobNode in jobNodes)
+            {
+                // 获取岗位名称
+                string jobTitle = jobNode.SelectSingleNode(".//span[@class='jobName']/a").InnerText.Trim();
+
+                // 获取公司信息
+                string company = jobNode.SelectSingleNode(".//div[@class='searchResultCompanyname']/a").InnerText.Trim();
+
+                // 获取HR联系方式
+                string hrContact = jobNode.SelectSingleNode(".//div[@class='searchResultCompanyname']/span").InnerText.Trim();
+
+                // 获取HR姓氏
+                string hrLastName = hrContact.Split(' ')[0];
+
+                // 获取岗位职责
+                string jobDescription = jobNode.SelectSingleNode(".//p[@class='searchResultJobdescription']").InnerText.Trim();
+
+                // 输出结果
+                Console.WriteLine("岗位名称: " + jobTitle);
+                Console.WriteLine("公司信息: " + company);
+                Console.WriteLine("HR联系方式: " + hrContact);
+                Console.WriteLine("HR姓氏: " + hrLastName);
+                Console.WriteLine("岗位职责: " + jobDescription);
+                Console.WriteLine();
+            }
+        }
+    }
+}
--- ConvertApiSVG/Program.cs
+++ ConvertApiSVG/Program.cs
@@ -1,4 +1,7 @@
 ﻿using ConvertApiDotNet;
+using System.Net;
+using System.Xml;
+using HtmlAgilityPack
 
 namespace ConvertApiSVG
 {
@@ -6,16 +9,52 @@ namespace ConvertApiSVG
     {
         static void Main(string[] args)
         {
-            async static Task Main(string[] args)
+             static void Main(string[] args)
             {
 
-                string From = @"C:\Users\Prozkb\Desktop\WJ\1-8.dwg";
-                string To = @"C:\Users\Prozkb\Desktop\WJ\";
+                string keyword = "销售";
+                string url = "https://www.zhaopin.com/";
 
-                ConvertApi convertApi = new ConvertApi("7a2BOAw4bHQVjzU3");
+                // 创建Web客户端
+                WebClient client = new WebClient();
+                client.Encoding = System.Text.Encoding.UTF8;
 
-                var convert = await convertApi.ConvertAsync("dwg", "svg", new ConvertApiFileParam("File", From));
-                await convert.SaveFilesAsync(To);
+                // 下载页面内容
+                string html = client.DownloadString(url);
+
+                // 使用HtmlAgilityPack解析HTML
+                HtmlDocument doc = new HtmlDocument();
+                doc.LoadHtml(html);
+
+                // 获取岗位列表
+                HtmlNodeCollection jobNodes = doc.DocumentNode.SelectNodes("//div[@class='listBox']/ul/li");
+
+                // 遍历岗位列表
+                foreach (HtmlNode jobNode in jobNodes)
+                {
+                    // 获取岗位名称
+                    string jobTitle = jobNode.SelectSingleNode(".//span[@class='jobName']/a").InnerText.Trim();
+
+                    // 获取公司信息
+                    string company = jobNode.SelectSingleNode(".//div[@class='searchResultCompanyname']/a").InnerText.Trim();
+
+                    // 获取HR联系方式
+                    string hrContact = jobNode.SelectSingleNode(".//div[@class='searchResultCompanyname']/span").InnerText.Trim();
+
+                    // 获取HR姓氏
+                    string hrLastName = hrContact.Split(' ')[0];
+
+                    // 获取岗位职责
+                    string jobDescription = jobNode.SelectSingleNode(".//p[@class='searchResultJobdescription']").InnerText.Trim();
+
+                    // 输出结果
+                    Console.WriteLine("岗位名称: " + jobTitle);
+                    Console.WriteLine("公司信息: " + company);
+                    Console.WriteLine("HR联系方式: " + hrContact);
+                    Console.WriteLine("HR姓氏: " + hrLastName);
+                    Console.WriteLine("岗位职责: " + jobDescription);
+                    Console.WriteLine();
+                }
             }
         }
     }
