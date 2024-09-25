5aab7f393b2ce9d61aefad83eff1498d0f005362
ZT158\dyzhukb <dyzhukb@chint.com>
2024-09-25 16:49:16 +0800

    null

---
+++ 组件扫描/Program.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+namespace 组件扫描
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+
+    public class Extension
+    {
+        public static IServiceCollection serviceDescriptors<T>(this IServiceCollection serviceDescriptors)
+        {
+            var types = typeof(T).Assembly.GetTypes();
+
+            foreach (var item in types)
+            {
+                //GetCustomAttributes,只用来收集InjectionAttribute的标签
+                var injection = item.GetCustomAttribute<InjectionAttribute>();
+                if (injection!.ServiceType == null)
+                {
+                    serviceDescriptors.Add(new ServiceDescriptor(item, item, injection.Lifetime));
+                }
+            }
+        }
+    }
+
+
+    [AttributeUsage(AttributeTargets.Class)]
+    public class InjectionAttribute : Attribute
+    {
+        public Type? Type { get; set; }
+
+        public ServiceLifetime Lifetime { get; set; } = ServiceLifetime.Transient;
+    }
+}
