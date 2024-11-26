0e4e68d5ec6eb8fef1a4e2d548e99e8873796b09
ZT158\dyzhukb <dyzhukb@chint.com>
2024-11-26 20:27:30 +0800

    fix

--- 控制行高3/Program.cs
+++ 控制行高3/Program.cs
@@ -7,24 +7,20 @@
             // 初始化数据库中的功能单元
             List<FuncUnit> funcUnits = new List<FuncUnit>
             {
-                new FuncUnit { Id = 1, Name = "功能单元1", Modulus = "12", SortOrder = 1 },
-                new FuncUnit { Id = 2, Name = "功能单元2", Modulus = "8/3", SortOrder = 2 },
-
-                //new FuncUnit { Id = 3, Name = "功能单元3", Modulus = "12", SortOrder = 3 }
+                new FuncUnit { Id = 42, Name = "8/2_01", Modulus = "8/2", SortOrder = 1 },
+                new FuncUnit { Id = 43, Name = "8/2_02", Modulus = "8/2", SortOrder = 2 },
+                new FuncUnit { Id = 44, Name = "8/2_03", Modulus = "8/2", SortOrder = 3 },
+                new FuncUnit { Id = 45, Name = "8/2_04", Modulus = "8/2", SortOrder = 4 },
+                new FuncUnit { Id = 46, Name = "8/2_05", Modulus = "8/2", SortOrder = 5 },
+                new FuncUnit { Id = 47, Name = "12_01", Modulus = "12", SortOrder = 6 },
+                new FuncUnit { Id = 48, Name = "12_02", Modulus = "12", SortOrder = 7 },
             };
 
             // 批量新增功能单元
             AddFuncUnits(funcUnits, new List<FuncUnit>
             {
-                new FuncUnit { Name = "新增功能单元1", Modulus = "15" },
                 new FuncUnit { Name = "新增功能单元1", Modulus = "8/2" },
-                new FuncUnit { Name = "新增功能单元2", Modulus = "8/2" },
-                new FuncUnit { Name = "新增功能单元3", Modulus = "8/3" },
-                new FuncUnit { Name = "新增功能单元3", Modulus = "8/3" },
-                new FuncUnit { Name = "新增功能单元3", Modulus = "8/3" },
             });
-
-            // 新增功能单元逻辑
 
             // 打印结果
             foreach (var unit in funcUnits.OrderBy(u => u.SortOrder))
@@ -56,11 +52,14 @@
                         // 当前相同模数的行已使用的数量
                         int currentCountInRow = existingUnits.Count;
 
-                        if (currentCountInRow < maxUnitsPerRow)
+                        //这里是取模，就是看当前的这个类型的模数是不是已经是"满的"
+                        //比如:数据库中已经存在的"8/2"是5个，那么再新增一个，那么5%2=1 是大于0的，说明还没有"满",那就应该再原来的"8/2"上继续排序
+                        var oushu = currentCountInRow % maxUnitsPerRow;
+                        if (oushu > 0 || currentCountInRow < maxUnitsPerRow)
                         {
                             // 在最后一个相同模数的行之后插入
                             var lastSameModulusUnit = existingUnits.Last();
-                            int insertIndex = oldFuncUnits.IndexOf(lastSameModulusUnit) + 1;
+                            int insertIndex = oldFuncUnits.OrderBy(x => x.SortOrder).ToList().IndexOf(lastSameModulusUnit) + 1;
 
                             oldFuncUnits.Insert(insertIndex, new FuncUnit
                             {
@@ -73,6 +72,7 @@
                             // 更新后续元素的 SortOrder
                             for (int i = insertIndex + 1; i < oldFuncUnits.Count; i++)
                             {
+                                var name = oldFuncUnits[i].Name;
                                 oldFuncUnits[i].SortOrder++;
                             }
 
