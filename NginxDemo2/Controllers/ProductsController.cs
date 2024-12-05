a1c2999e0b80e31261925939b9f405256bd05333
ZhuKaiBin <912549963@qq.com>
2024-12-05 23:02:29 +0800

    nginx

---
+++ NginxDemo2/Controllers/ProductsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NginxDemo2.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing",
+            "Bracing",
+            "Chilly",
+            "Cool",
+            "Mild",
+            "Warm",
+            "Balmy",
+            "Hot",
+            "Sweltering",
+            "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        [ResponseCache(Duration = 600, Location = ResponseCacheLocation.Client)]
+        public int Get()
+        {
+            return 1;
+        }
+    }
+}
---
+++ NginxDemo2/Program.cs
@@ -0,0 +1,52 @@
+namespace NginxDemo2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllers();
+            builder.Services.AddRazorPages();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            //builder.Services.AddControllers().AddApplicationPart(typeof(WeatherForecastController).Assembly);
+
+            ////1.集成Identity上下文
+            //builder.Services.AddDbContext<MesDbContext>(op =>
+            //{
+            //    //2.集成mysql
+            //    op.UseMySql("Server=localhost;Port=3306;Database=MES_Identity;uid=root;Pwd=123456;",
+            //                  new MySqlServerVersion(new Version(8, 0, 39))); // 设置 MySQL 版本号
+            //});
+
+            ////注册IUserManager/ SignManager
+            //builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<MesDbContext>();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+                app.UseSwagger();  // 启用 Swagger
+                app.UseSwaggerUI();  // 启用 Swagger UI
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapRazorPages();
+            // 确保使用路由并启用控制器映射
+            app.MapControllers();
+            app.Run();
+        }
+    }
+}
