c60f0f8292ea907d4464ab363a8010d12f046a9d
ZT158\dyzhukb <dyzhukb@chint.com>
2024-12-17 09:25:28 +0800

    测试HTTP 复用TCP的实列

---
+++ HTTP2VersionDemo/Program.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace HTTP2VersionDemo
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // 设置服务集合（依赖注入容器）
+            var serviceProvider = new ServiceCollection()
+                .AddHttpClient() // 注册 IHttpClientFactory
+                .BuildServiceProvider();
+
+            // 通过依赖注入获取 IHttpClientFactory 实例
+            var httpClientFactory = serviceProvider.GetRequiredService<IHttpClientFactory>();
+
+
+            var client = httpClientFactory.CreateClient();
+
+            // 发起请求
+            try
+            {
+                for (int i = 0; i < 100000000; i++)
+                {
+
+                    var response = await client.GetAsync("https://c-design.oss-cn-hangzhou.aliyuncs.com/env_staging/graphics/materials/dwg/5048DB1602.dwg");
+                    response.EnsureSuccessStatusCode(); // 确保响应成功
+
+                    var resVersion = response.Version;
+
+                    //// 读取并输出响应内容
+                    //var content = await response.Content.ReadAsStringAsync();
+
+                    Console.WriteLine($"HTTP Version: {response.Version}");
+                    //Console.WriteLine(content);
+                }
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine($"请求失败: {e.Message}");
+            }
+
+        }
+    }
+}
