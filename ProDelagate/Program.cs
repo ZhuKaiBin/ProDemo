be40f46881a3bde6e8fc475967b933e7f1083c88
ZT158\dyzhukb <dyzhukb@chint.com>
2025-01-16 17:29:37 +0800

    fix

--- ProDelagate/Program.cs
+++ ProDelagate/Program.cs
@@ -6,14 +6,35 @@ namespace ProDelagate
     {
         static void Main(string[] args)
         {
+            {
+                var publisher = new Publisher();
+                var subscriber = new Subscriber();
+                publisher.PrintEvent += subscriber.OrderHandle;
 
-            Test test = new Test();
-            test.delegatePrint += p1;
-            test.delegatePrint += p2;
+                publisher.PrintEvent = null;  // ①这里直接修改了委托链，后续的事件将无法被触发
+                // ②或者替换委托链，导致事件处理方法被覆盖
+                publisher.PrintEvent = new delegateNoEventPrint((me) => Console.WriteLine("New handler"));
 
-            test.Run();
+                //①② 这两种情况，就好比是没有走正常的发布订阅，半路杀出来个程咬金，然后破坏我们的委托链
+                //可能导致后边触发事件失败，执行失败，如果执行①，那么后面的触发事件是不起作用的
 
-            Console.ReadKey();
+                publisher.Trigger("开始发布事件");
+
+            }
+
+            {
+                Test test = new Test();
+                test.delegatePrint += p1;
+                test.delegatePrint += p2;
+
+                //test.delegatePrint = null;//这里编译是报错的：事件“Program.Test.delegatePrint”只能出现在 += 或 -= 的左边(从类型“Program.Test”中使用时除外)
+
+
+
+                test.Run();
+
+                Console.ReadKey();
+            }
         }
 
         static void p1()
@@ -45,6 +66,31 @@ namespace ProDelagate
                                 //调用Run，就都会把绑定在delegatePrint上的方法都调用一遍
 
                 //委托就是一个集合，然后调用这个集合，就会调用集合里面的所有方法
+            }
+        }
+
+        public delegate void delegateNoEventPrint(string message);
+        class Publisher
+        {
+            public delegateNoEventPrint PrintEvent;
+
+            public void Trigger(string message)
+            {
+                PrintEvent?.Invoke(message);
+            }
+        }
+
+        public class Subscriber
+        {
+            public void OrderHandle(string message)
+            {
+                Console.WriteLine($"Received message: {message}");
+            }
+
+
+            public void DeliverHandle(string message)
+            {
+                Console.WriteLine($"Received message: {message}");
             }
         }
 
--- ServiceCollectionDemo/Program.cs
+++ ServiceCollectionDemo/Program.cs
@@ -7,26 +7,20 @@ namespace ServiceCollectionDemo
     {
         static void Main(string[] args)
         {
-           var d=  DateTimeOffset.UtcNow;
 
+            // 创建一个服务集合（就像餐厅的厨房）
+            var serviceCollection = new ServiceCollection();
+            // 在厨房中注册服务（例如：Greeter）
+            serviceCollection.AddSingleton<IGreeter, Greeter>();
 
-            var d2= DateTimeOffset.Now;
+            // 构建 IServiceProvider（就像雇佣了一个服务员）
+            var serviceProvider = serviceCollection.BuildServiceProvider();
 
+            // 服务员通过 IServiceProvider 获取服务（就像服务员从厨房拿菜）
+            var greeter = serviceProvider.GetService<IGreeter>();
 
-            //// 创建一个服务集合（就像餐厅的厨房）
-            //var serviceCollection = new ServiceCollection();
-            //// 在厨房中注册服务（例如：Greeter）
-            //serviceCollection.AddSingleton<IGreeter, Greeter>();
-
-            //// 构建 IServiceProvider（就像雇佣了一个服务员）
-            //var serviceProvider = serviceCollection.BuildServiceProvider();
-
-            //// 服务员通过 IServiceProvider 获取服务（就像服务员从厨房拿菜）
-            //var greeter = serviceProvider.GetService<IGreeter>();
-
-            //// 使用服务（就像客人享用餐品）
-            //greeter.Greet("World");
-
+            // 使用服务（就像客人享用餐品）
+            greeter.Greet("World");
 
 
             //那么就是这样，然后要用到Greet这个方法的地方，就直接用构造函数引入就行了，
